23b4feb208fa2fd2b0c91d3a0b87e0a1999317c0
izabayo7 <cedricizabayo7@gmail.com>
2023-01-15 20:41:04 +0200

    chore: stopped using proxy

--- src/services/index.js
+++ src/services/index.js
@@ -6,60 +6,60 @@ export const API_URL =
 
 class AppServices {
   login(body) {
-    return axios.post(`/api/login`, body);
+    return axios.post(`${API_URL}/login`, body);
   }
 
   updateUser(body, id) {
-    return axios.put(`/api/users/${id}`, body);
+    return axios.put(`${API_URL}/users/${id}`, body);
   }
 
   getCurrentUser() {
-    return axios.get(`/api/user`);
+    return axios.get(`${API_URL}/user`);
   }
 
   register(body) {
-    return axios.post(`/api/users`, body);
+    return axios.post(`${API_URL}/users`, body);
   }
 
   deleteUser() {
-    return axios.delete(`/api/users`);
+    return axios.delete(`${API_URL}/users`);
   }
 
   getItems(path) {
-    return axios.get(`/api/${path}`);
+    return axios.get(`${API_URL}/${path}`);
   }
 
   createItem(path, body) {
-    return axios.post(`/api/${path}`, body);
+    return axios.post(`${API_URL}/${path}`, body);
   }
 
   updateItem(path, body) {
-    return axios.put(`/api/${path}`, body);
+    return axios.put(`${API_URL}/${path}`, body);
   }
 
   deleteItem(path) {
-    return axios.delete(`/api/${path}`);
+    return axios.delete(`${API_URL}/${path}`);
   }
 
 
   verifyAccount(id) {
-    return axios.post(`/api/verification/${id}`);
+    return axios.post(`${API_URL}/verification/${id}`);
   }
 
   updatePasswordReset(body) {
-    return axios.post(`/api/auth/updated-password-reset`, body);
+    return axios.post(`${API_URL}/auth/updated-password-reset`, body);
   }
 
   createPasswordReset(body) {
-    return axios.post(`/api/auth/create-password-reset`, body);
+    return axios.post(`${API_URL}/auth/create-password-reset`, body);
   }
 
   getPasswordReset(token) {
-    return axios.get(`/api/auth/${token}`);
+    return axios.get(`${API_URL}/auth/${token}`);
   }
 
   getUsers() {
-    return axios.get(`/api/users`);
+    return axios.get(`${API_URL}/users`);
   }
 
 }
